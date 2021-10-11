a8395c80a8c83fa294873c6989013db655615d7c
gonygon <gudrhs2452@naver.com>
2021-10-11 16:32:13 +0900

    add

--- R/Reliability_test2.R
+++ R/Reliability_test2.R
@@ -5,15 +5,15 @@
 #' @keywords Reliability_test2
 #' @export
 
-Reliability_test2<-function(a,b,c){
-  n_star<-ceiling(sqrt(a*(b/c)))
+Reliability_test2<-function(r,c_r,c_i){
+  n_star<-ceiling(sqrt(r*(c_r/c_i)))
   n_s<-n_star+30
   n<-c(1:n_s)
-  k<-(a/n)*b+n*c
-  r<-min(k)
-  t<-which.min(k)
-  x<-plot(n,k, type = 'b',col="red")
-  Reliability<-c(t,r,x)
+  C_n<-(r/n)*c_r+n*c_i
+  best_cost<-min(C_n)
+  best_n<-which.min(C_n)
+  x<-plot(n,C_n, type = 'l',col="red")
+  Reliability<-c(best_n,best_cost,x)
   names(Reliability)<-c("최적점검횟수","총 기대비용")
   Reliability
 }
