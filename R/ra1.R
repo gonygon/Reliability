19f3181da0e5c378121d114ef1323dd33607170f
gonygon <gudrhs2452@naver.com>
2021-10-19 22:00:09 +0900

    edit

---
+++ R/ra1.R
@@ -0,0 +1,17 @@
+#' ra1() Function
+#'
+#' 평균수명,고장률,단위시간동안 고장나지 않을 확률을 계산해주는 함수 입니다.
+#' x에는 데이터 값을 t에는 단위시간을 입력하면 됩니다
+#' @param x: 데이터값,t:단위시간
+#' @keywords ra1
+#' @export
+
+ra1<-function(x,t){
+  M<-mean(x)
+  L<-1/mean(x)
+  R<-exp(-((1/mean(x))*t))
+  plot<-boxplot(x)
+  Reliability<-c(M,L,R,plot)
+  names(Reliability)<-c("평균수명","고장률","단위시간t동안 고장나지 않을 확률")
+  Reliability
+}
--- R/ra2.R
+++ R/ra2.R
@@ -1,13 +1,13 @@
-#' Reliability_test2() Function
+#' ra2() Function
 #'
 #' 비용최적화를 위한 R 패키지 함수이며 (r,Cr,Ci)형태의 함수를 유지하고 있습니다
 #' r에는 단위시간당 평균고장횟수를 입력 c_r에는 고장시수리비용 c_i에는 점검비용을 입력합니다.
 #' 따라서 이를 통해 최적 점검횟수와 총 기대비용이 구해지며 그래프를 통해 기대비용이 어떠한 분포를 이루는지 알 수 있습니다.
 #' @param r:평균고장횟수 Cr: 고장시 수리비용 Ci:점검비용
-#' @keywords Reliability_test2
+#' @keywords ra2
 #' @export
 
-Reliability_test2<-function(r,Cr,Ci){
+ra2<-function(r,Cr,Ci){
   n_star<-ceiling(sqrt(r*(Cr/Ci)))
   n_s<-n_star+30
   n<-c(1:n_s)
